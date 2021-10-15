46deb1afa1fd69aa707ee54f71de2b972c771a59
adrian <adrian.rebollar.06@gmail.com>
2021-10-15 12:28:28 +0200

    añadir grupo favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 		
-		System.out.println("Adrian Rebollar Baños");
 		System.out.println("Leon, 2000");
 		System.out.println("Leon Windows 10 Pro Education");
+		System.out.println("Mi grupo favorito es La raíz");
 		
 	}
 
